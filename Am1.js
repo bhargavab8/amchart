bbb075976411a0b3f7e40f88e737e661ed57e01b
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2020-05-14 12:03:12 +0530

    Add files via upload

---
+++ Am1.js
@@ -0,0 +1,42 @@
+(function() { 
+    let template = document.createElement("template");
+    template.innerHTML = `
+    <style>
+    #chartdiv {
+      width: 100%;
+      height: 500px;
+    }
+    
+    </style>
+    
+    <!-- Resources -->
+    <script src="https://www.amcharts.com/lib/4/core.js"></script>
+    <script src="https://www.amcharts.com/lib/4/charts.js"></script>
+    <script src="https://www.amcharts.com/lib/4/themes/animated.js"></script>
+        <div id="chartdiv"></div>
+    `;
+    class ColoredBox extends HTMLElement {
+        constructor() {
+            super(); 
+            let shadowRoot = this.attachShadow({mode: "open"});
+            shadowRoot.appendChild(template.content.cloneNode(true));
+            this.addEventListener("click", event => {
+                var event = new Event("onClick");
+                this.dispatchEvent(event);
+            });
+            this._props = {};
+        }
+        onCustomWidgetBeforeUpdate(changedProperties) {
+            this._props = { ...this._props, ...changedProperties };
+        }
+        onCustomWidgetAfterUpdate(changedProperties) {
+            if ("color" in changedProperties) {
+                this.style["background-color"] = changedProperties["color"];
+            }
+            if ("opacity" in changedProperties) {
+                this.style["opacity"] = changedProperties["opacity"];
+            }
+        }
+    }
+    customElements.define("com-sap-sample-coloredbox", ColoredBox);
+})();
