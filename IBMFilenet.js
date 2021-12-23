448423c6f8d090788ea866690184506297ee29dd
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2021-12-23 17:04:08 +0530

    Update IBMFilenet.js

--- IBMFilenet.js
+++ IBMFilenet.js
@@ -1,31 +1,7 @@
 (function() { 
     let template = document.createElement("template");
     template.innerHTML = `
-    <head>
-       
-    
-        <style>
-           div { font:17px 'Calibri'; }
-    table, th, td {
-                border: solid 1px #ddd;
-                border-collapse: collapse;
-                padding: 2px 3px;
-                text-align: center;
-            }
-            th {
-                font-weight:bold;
-            }
-            
-        </style>
-    </head>
-    <body>
-        
-        <div id='showTable'></div>
-    </body>
-    ;
-    <script>
-             
-    </script>
+    <div id="showTable" class="Chart1" style="width:800px; height:600px" ></div>
     `;
     class TableJSON extends HTMLElement {
         constructor() {
@@ -45,7 +21,67 @@
             this._props = { ...this._props, ...changedProperties };
             var myprops = this._props       
             var val = myprops.value;  
-            this.$script.innerHTML=' var oXHR = new XMLHttpRequest();oXHR.onreadystatechange = reportStatus;oXHR.open("GET",'+val+' , true);file.oXHR.send(); function reportStatus() { if (oXHR.readyState == 4) {createTableFromJSON(this.responseText);}}function createTableFromJSON(jsonData) { var arrBirds = [];arrBirds = JSON.parse(jsonData);var col = []; for (var i = 0; i < arrBirds.length; i++) {for (var key in arrBirds[i]) {if (col.indexOf(key) === -1) {col.push(key);}}}var table = document.createElement("table");var tr = table.insertRow(-1);for (var i = 0; i < col.length; i++) { var th = document.createElement("th");th.innerHTML = col[i];tr.appendChild(th);}for (var i = 0; i < arrBirds.length; i++) { tr = table.insertRow(-1);for (var j = 0; j < col.length; j++) {var tabCell = tr.insertCell(-1);tabCell.innerHTML = arrBirds[i][col[j]];}}var divContainer = document.getElementById("showTable");divContainer.innerHTML = "";divContainer.appendChild(table);};';
+    // Create XMLHttpRequest object.
+    var oXHR = new XMLHttpRequest();
+
+    // Initiate request.
+    oXHR.onreadystatechange = reportStatus;
+    oXHR.open("GET", val, true);  // get json file.
+    oXHR.send();
+
+    function reportStatus() {
+        if (oXHR.readyState == 4) {		// Check if request is complete.
+
+            // Create an HTML table using response from server.
+            createTableFromJSON(this.responseText);
+        }
+    }
+
+    // Create an HTML table using the JSON data.
+    function createTableFromJSON(jsonData) {
+        var arrBirds = [];
+        arrBirds = JSON.parse(jsonData); 	// Convert JSON to array.
+
+        var col = [];
+        for (var i = 0; i < arrBirds.length; i++) {
+            for (var key in arrBirds[i]) {
+                if (col.indexOf(key) === -1) {
+                    col.push(key);
+                }
+            }
+        }
+
+        // Create a dynamic table.
+        var table = document.createElement("table");
+
+        // Create table header.
+
+        var tr = table.insertRow(-1);                   // Table row.
+
+        for (var i = 0; i < col.length; i++) {
+            var th = document.createElement("th");      // Table header.
+            th.innerHTML = col[i];
+            tr.appendChild(th);
+        }
+
+        // Add JSON to the table rows.
+        for (var i = 0; i < arrBirds.length; i++) {
+
+            tr = table.insertRow(-1);
+
+            for (var j = 0; j < col.length; j++) {
+                var tabCell = tr.insertCell(-1);
+                tabCell.innerHTML = arrBirds[i][col[j]];
+            }
+        }
+
+        // Finally, add the dynamic table to a container.
+        var divContainer = document.getElementById("showTable");
+        divContainer.innerHTML = "";
+        divContainer.appendChild(table);
+    };
+
+            
         }
     }
     customElements.define("com-sap-sample-tablejson", TableJSON);
