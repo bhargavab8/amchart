e586044cc2b3bc16002897f0954e076882bb32c6
bhargavab8 <56081458+bhargavab8@users.noreply.github.com>
2021-12-23 13:57:26 +0530

    Update IBMFilenet.js

--- IBMFilenet.js
+++ IBMFilenet.js
@@ -24,69 +24,6 @@
     </body>
     ;
     <script>
-    
-    
-        // Create XMLHttpRequest object.
-        
-        var oXHR = new XMLHttpRequest();
-    
-        // Initiate request.
-        oXHR.onreadystatechange = reportStatus;
-        oXHR.open("GET","https://raw.githubusercontent.com/bhargavab8/amchart/master/Data.json" , true);  // get json file.
-        oXHR.send();
-    
-        function reportStatus() {
-            if (oXHR.readyState == 4) {		// Check if request is complete.
-    
-                // Create an HTML table using response from server.
-                createTableFromJSON(this.responseText);
-            }
-        }
-    
-        // Create an HTML table using the JSON data.
-        function createTableFromJSON(jsonData) {
-            var arrBirds = [];
-            arrBirds = JSON.parse(jsonData); 	// Convert JSON to array.
-    
-            var col = [];
-            for (var i = 0; i < arrBirds.length; i++) {
-                for (var key in arrBirds[i]) {
-                    if (col.indexOf(key) === -1) {
-                        col.push(key);
-                    }
-                }
-            }
-    
-            // Create a dynamic table.
-            var table = document.createElement("table");
-    
-            // Create table header.
-    
-            var tr = table.insertRow(-1);                   // Table row.
-    
-            for (var i = 0; i < col.length; i++) {
-                var th = document.createElement("th");      // Table header.
-                th.innerHTML = col[i];
-                tr.appendChild(th);
-            }
-    
-            // Add JSON to the table rows.
-            for (var i = 0; i < arrBirds.length; i++) {
-    
-                tr = table.insertRow(-1);
-    
-                for (var j = 0; j < col.length; j++) {
-                    var tabCell = tr.insertCell(-1);
-                    tabCell.innerHTML = arrBirds[i][col[j]];
-                }
-            }
-    
-            // Finally, add the dynamic table to a container.
-            var divContainer = document.getElementById("showTable");
-            divContainer.innerHTML = "";
-            divContainer.appendChild(table);
-            
-        };
              
     </script>
     `;
@@ -109,7 +46,6 @@
             var myprops = this._props       
             var val = myprops.value;  
             this.$script.innerHTML=' var oXHR = new XMLHttpRequest();oXHR.onreadystatechange = reportStatus;oXHR.open("GET",'+val+' , true);file.oXHR.send(); function reportStatus() { if (oXHR.readyState == 4) {createTableFromJSON(this.responseText);}}function createTableFromJSON(jsonData) { var arrBirds = [];arrBirds = JSON.parse(jsonData);var col = []; for (var i = 0; i < arrBirds.length; i++) {for (var key in arrBirds[i]) {if (col.indexOf(key) === -1) {col.push(key);}}}var table = document.createElement("table");var tr = table.insertRow(-1);for (var i = 0; i < col.length; i++) { var th = document.createElement("th");th.innerHTML = col[i];tr.appendChild(th);}for (var i = 0; i < arrBirds.length; i++) { tr = table.insertRow(-1);for (var j = 0; j < col.length; j++) {var tabCell = tr.insertCell(-1);tabCell.innerHTML = arrBirds[i][col[j]];}}var divContainer = document.getElementById("showTable");divContainer.innerHTML = "";divContainer.appendChild(table);};';
-
         }
     }
     customElements.define("com-sap-sample-tablejson", TableJSON);
